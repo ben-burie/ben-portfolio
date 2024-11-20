bdd8ce71b4a3d70d6ab7cbbcbb4e4b4c5e1de67b
ben-burie <benburie22@gmail.com>
2024-11-20 09:06:56 -0600

    Add first changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
