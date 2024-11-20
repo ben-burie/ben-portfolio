4c1bcfaf9713a679fb57b96a1b80675b8e2163b9
ben-burie <benburie22@gmail.com>
2024-11-20 09:19:47 -0600

    Add test styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import './styles.css'
 
 const App = () => (
   <div>
