950e491890015aec70f521e56f37ff425fe202e2
ben-burie <benburie22@gmail.com>
2024-11-21 10:15:05 -0600

    Added email and clipboard copy functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
+
+  const [tooltipStyle, setTooltipStyle] = useState({ display: 'none', left: 0, top: 0 });
+  const [tooltipText, setTooltipText] = useState('Copy to Clipboard');
+
+  const handleMouseEnter = (e) => {
+    setTooltipStyle({
+      display: 'block',
+      left: e.pageX + 10, // Offset the tooltip slightly from the cursor
+      top: e.pageY + 10,
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    setTooltipStyle({
+      display: 'block',
+      left: e.pageX + 10,
+      top: e.pageY + 10,
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setTooltipStyle({ display: 'none' });
+    setTooltipText('Copy to Clipboard');
+  };
+
+  const handleClick = () => {
+    copyToClipboard('example@gmail.com');
+    setTooltipText('Copied!');
+  };
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text).catch((err) => {
+      console.error('Failed to copy: ', err);
+    });
+  };
 
   const scrollToSection = (id) => {
     const section = document.getElementById(id);
@@ -14,6 +49,17 @@ const Header = () => {
 
   return (
     <section id="header" className="section header">
+      <div className="ext-links">
+        <a href="#" 
+        onMouseEnter={handleMouseEnter}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        onClick={handleClick}>example@gmail.com</a>
+        <button></button>
+        <div className="tooltip" style={tooltipStyle}>
+          {tooltipText}
+        </div>
+      </div>
       <nav>
         <a href="#about" onClick={(e) => { e.preventDefault(); scrollToSection('about'); }}>About</a>
         <a href="#projects" onClick={(e) => { e.preventDefault(); scrollToSection('projects'); }}>Projects</a>
