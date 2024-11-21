1606c2b90d76eb07c312f657d2b5cd035e1df8aa
ben-burie <benburie22@gmail.com>
2024-11-21 09:39:42 -0600

    Edited home page layout, added image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
-const Header = () => (
-  <section id="header" className="section header">
-    <h1>My Coding Portfolio</h1>
-    <nav>
-      <a href="#about">About</a>
-      <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
-    </nav>
-  </section>
-);
+const Header = () => {
+
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({
+        behavior: 'smooth', // Smooth scrolling
+        block: 'start',     // Align the top of the section with the viewport
+      });
+    }
+  };
+
+  return (
+    <section id="header" className="section header">
+      <nav>
+        <a href="#about" onClick={(e) => { e.preventDefault(); scrollToSection('about'); }}>About</a>
+        <a href="#projects" onClick={(e) => { e.preventDefault(); scrollToSection('projects'); }}>Projects</a>
+        <a href="#contact" onClick={(e) => { e.preventDefault(); scrollToSection('contact'); }}>Contact</a>
+      </nav>
+      <div className="header-content">
+        <h1>Ben Burie</h1>
+        <h3>Aspiring Software Engineer</h3>
+      </div>
+      <img src="images\profile-circle.png" alt="profile" className="header-image"></img>
+    </section>
+  );
+};
 
 export default Header;
