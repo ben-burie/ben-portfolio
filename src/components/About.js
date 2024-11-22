f01bec37afe1bef1c512c34f387245192bef945a
ben-burie <benburie22@gmail.com>
2024-11-21 22:46:04 -0600

    Updated about page - added text

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const About = () => (
   <section id="about" className="section about">
-    <h2>About Me</h2>
-    <p>I'm a passionate developer with expertise in React and JavaScript...</p>
+    <div className ="about-header">
+        <h2>About</h2>
+        <h3>I am a <span>computer science</span> student with a <span>passion</span> for 
+        <span> learning</span> both inside and outside of the classroom.</h3>
+        <ul>
+            <li><h3>Software <span>Development</span></h3></li>
+            <li><h3>Web <span>Development</span></h3></li>
+            <li><h3>Data <span>Science</span></h3></li>
+            <li><h3>Machine <span>Learning</span></h3></li>
+        </ul>
+    </div>
   </section>
 );
 
